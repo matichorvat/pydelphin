f5817088a02c6e5d4400c0e409b46e5b22d1f002
Michael Wayne Goodman <goodman.m.w@gmail.com>
2014-09-12 17:52:02 +0800

    Add preliminary tikz exporter.

--- delphin/mrs/exporters.py
+++ delphin/mrs/exporters.py
@@ -31,3 +31,54 @@ def xmrs_to_nxd3(xmrs):
     from networkx.readwrite import json_graph
     data = json_graph.node_link_data(xmrs._graph)
     return json.dumps(data, default=lambda x: str(x), indent=2)
+
+def dmrs_to_tikz(xmrs):
+    header = '''\\documentclass{article}
+\\usepackage{tikz}
+\\usetikzlibrary{positioning}
+\\begin{document}
+\\begin{tikzpicture}[
+  font=\\sffamily,
+  xnode/.style={draw,rectangle,fill=blue!20},
+  qnode/.style={draw},
+  to/.style={->,thick,rounded corners=2pt,font=\\sffamily\\footnotesize},
+  every node/.style={semithick,text centered,minimum height=1.6em,minimum width=6em},
+  node distance=5pt]'''
+    lines = [header]
+    prev_nid = None
+    for ep in xmrs.eps:
+        if ep.is_quantifier():
+            continue
+        pos = '' if prev_nid is None else '[right=10pt of {}]'.format(prev_nid)
+        lines.append(
+            '\\node[{shape}] ({id}) {pos} {{\\verb={text}=}};'
+            .format(
+                shape='xnode',
+                id=ep.nodeid,
+                pos=pos,
+                text=ep.pred
+            )
+        )
+        prev_nid = ep.nodeid
+    for ep in xmrs.eps:
+        if not ep.is_quantifier():
+            continue
+        pos = '[below=30pt of {}]'.format(xmrs.get_nodeid(ep.iv))
+        lines.append(
+            '\\node[{shape}] ({id}) {pos} {{\\verb={text}=}};'
+            .format(
+                shape='xnode',
+                id=ep.nodeid,
+                pos=pos,
+                text=ep.pred
+            )
+        )
+    for link in xmrs.links:
+        if link.start == 0: continue
+        linkdir = 'south' if xmrs.get_ep(link.start).is_quantifier() else 'north'
+        lines.append(
+            '\\draw[to] ({start}.north) -- ++(0,1) -| ({end}.{dir});'
+            .format(start=link.start, end=link.end, dir=linkdir)
+        )
+    lines.append('\\end{tikzpicture}\n\\end{document}')
+    return '\n'.join(lines)
